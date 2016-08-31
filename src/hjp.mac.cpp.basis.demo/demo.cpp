c782bda97306bdec7d9c4db6fd885e5b7136c471
hjpwhu <hjpwhu@qq.com>
2016-08-31 19:47:11 +0800

    ini

---
+++ src/hjp.mac.cpp.basis.demo/demo.cpp
@@ -0,0 +1,18 @@
+/*
+ * demo.cpp
+ *
+ *  Created on: Aug 31, 2016
+ *      Author: hjp
+ */
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "Hello, C++!" << endl;
+
+	return 0;
+}
+
+
+
